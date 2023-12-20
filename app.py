56125780c3565658842978cf039aee4055006c7f
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-20 15:19:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,48 +3,92 @@ from ftplib import FTP
 import subprocess
 
 # Placeholder functions for Divi page creation and WordPress interaction
-def create_divi_page(api_key, page_title, page_content):
-    # Implement logic to create a new Divi page
-    return "123"  # Placeholder ID, replace with actual logic
+def create_divi_page(api_key, page_title, page_content, uploaded_file):
+    try:
+        # Implement logic to create a new Divi page
+        # You can use the uploaded_file parameter to handle file uploads
+        return "123"  # Placeholder ID, replace with actual logic
+    except Exception as e:
+        st.error(f"Error creating Divi page: {e}")
+        return None
 
 def set_home_page(api_key, page_id):
-    # Implement logic to set the newly created page as the homepage
-    pass
+    try:
+        # Implement logic to set the newly created page as the homepage
+        pass
+    except Exception as e:
+        st.error(f"Error setting the homepage: {e}")
 
-def upload_to_ftp():
-    # Implement logic to upload files to WordPress through FTP
-    pass
+def upload_to_ftp(ftp_credentials, uploaded_file):
+    try:
+        # Implement logic to upload files to WordPress through FTP
+        # Use ftp_credentials dictionary for FTP connection details
+        pass
+    except Exception as e:
+        st.error(f"Error uploading files to FTP: {e}")
+
+# Apply some basic styling using HTML and CSS
+st.markdown("""
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+        }
+        .sidebar .sidebar-content {
+            background-color: #f5f5f5;
+            padding: 1rem;
+            border-radius: 10px;
+        }
+        .main {
+            padding: 2rem;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 # Streamlit app
 def main():
     st.title("WordPress Divi Page Creator")
 
-    # Get OpenAPI key
-    api_key = st.text_input("Enter your OpenAPI key")
+    # Sidebar for FTP credentials and OpenAI key
+    with st.sidebar:
+        st.subheader("FTP Credentials")
+        ftp_host = st.text_input("FTP Host")
+        ftp_user = st.text_input("FTP User")
+        ftp_password = st.text_input("FTP Password", type="password")
 
-    # Get the number of page sections
-    num_sections = st.number_input("Number of Page Sections", min_value=1, value=1)
+        st.subheader("OpenAI Key")
+        openai_key = st.text_input("Enter your OpenAI key")
 
-    # Loop through each section
-    for i in range(num_sections):
-        st.header(f"Section {i + 1}")
+    # Main content for Divi page creation
+    with st.container():
+        st.header("Divi Page Creation")
 
-        # Get page details for each section
-        page_title = st.text_input(f"Enter the title for Section {i + 1}")
-        page_content = st.text_area(f"Enter the content for Section {i + 1}", height=200)
+        # Get the number of page sections
+        num_sections = st.number_input("Number of Page Sections", min_value=1, value=1)
 
-        # Create Divi page for each section
-        if st.button(f"Create Divi Page for Section {i + 1}"):
-            page_id = create_divi_page(api_key, page_title, page_content)
+        # Loop through each section
+        for i in range(num_sections):
+            st.subheader(f"Section {i + 1}")
 
-            if page_id:
-                st.success(f"Divi page created successfully for Section {i + 1} with ID: {page_id}")
+            # Get page details for each section
+            page_title = st.text_input(f"Enter the title for Section {i + 1}")
+            page_content = st.text_area(f"Enter the content for Section {i + 1}", height=200)
 
-                # Set the new page as the homepage
-                set_home_page(api_key, page_id)
+            # File upload for each section
+            uploaded_file = st.file_uploader(f"Upload file for Section {i + 1}", type=["jpg", "jpeg", "png", "pdf"])
 
-                # Upload files to WordPress through FTP
-                upload_to_ftp()
+            # Create Divi page for each section
+            if st.button(f"Create Divi Page for Section {i + 1}"):
+                page_id = create_divi_page(openai_key, page_title, page_content, uploaded_file)
+
+                if page_id:
+                    st.success(f"Divi page created successfully for Section {i + 1} with ID: {page_id}")
+
+                    # Set the new page as the homepage
+                    set_home_page(openai_key, page_id)
+
+                    # Upload files to WordPress through FTP
+                    ftp_credentials = {'host': ftp_host, 'user': ftp_user, 'password': ftp_password}
+                    upload_to_ftp(ftp_credentials, uploaded_file)
 
 if __name__ == "__main__":
     main()
